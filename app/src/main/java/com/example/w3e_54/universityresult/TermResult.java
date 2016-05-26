7c6df6d7ee912a2fb64ffac9f10265fe929076c0
Anindya-Duti-Dhar <anindya_android@w3engineers.com>
2016-05-26 12:32:03 +0600

    First Push

---
+++ app/src/main/java/com/example/w3e_54/universityresult/TermResult.java
@@ -0,0 +1,12 @@
+package com.example.w3e_54.universityresult;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by w3e-54 on 1/27/16.
+ */
+public class TermResult extends AppCompatActivity {
+
+
+
+}
---
+++ app/src/main/java/com/example/w3e_54/universityresult/result.java
@@ -0,0 +1,7 @@
+package com.example.w3e_54.universityresult;
+
+/**
+ * Created by w3e-54 on 1/27/16.
+ */
+public class result {
+}
